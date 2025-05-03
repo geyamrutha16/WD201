5d510e5e13718d2747deb604c21b6be6b2e1eca2
Geyamrutha Poluru <shinchanharry148@gmail.com>
2025-05-03 21:46:41 +0530

    final_commit20

--- todo-app/app.js
+++ todo-app/app.js
@@ -21,7 +21,7 @@ router.get('/', async (req, res) => {
         const completed = todos.filter(todo => todo.completed);
 
         res.render('index', {
-            todos: fetchedTodos,
+            todos,
             overdue,
             dueToday,
             dueLater,

c182c7d19689c3b942ddc09c3d0209e4927604f1
Geyamrutha Poluru <shinchanharry148@gmail.com>
2025-05-03 21:18:23 +0530

    final_commit15

--- todo-app/app.js
+++ todo-app/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { Todo } = require('../models');
+const { Todo } = require('./models');
 const csrf = require('csurf');
 const csrfProtection = csrf({ cookie: true });
 

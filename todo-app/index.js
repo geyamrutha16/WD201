695489c116c3a502752b7123ba619d66c14c342d
Geyamrutha Poluru <shinchanharry148@gmail.com>
2025-05-04 13:06:00 +0530

    final_commit18

--- todo-app/index.js
+++ todo-app/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const { sequelize.Todo } = require('./models');
+const { sequelize, Todo } = require('./models');
 const csrf = require('csurf');
 const cookieParser = require('cookie-parser');
 
